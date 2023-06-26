d31dd34bc0fd51879a52516dad0e22431ed44404
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-06-26 21:20:59 +0700

    mod: port

--- index.js
+++ index.js
@@ -24,45 +24,42 @@
 
 // app.use("/api/compress", compressRoutes);
 
-
-
 // app.listen(8080, () => {
 //     console.log("listening on port 8080");
 // });
 
-import app from "./server.js"
+import app from "./server.js";
 
-import mongodb from "mongodb"
+import mongodb from "mongodb";
 
-import compressDAO from "./dao/compressDAO.js"
+import compressDAO from "./dao/compressDAO.js";
 
-import 'dotenv/config'
+import "dotenv/config";
 
-const MongoClient = mongodb.MongoClient
-const mongo_username = process.env.MONGO_USERNAME
-const mongo_password = process.env.MONGO_PASSWORD
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.ru5mees.mongodb.net/?retryWrites=true&w=majority`
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env.MONGO_USERNAME;
+const mongo_password = process.env.MONGO_PASSWORD;
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.ru5mees.mongodb.net/?retryWrites=true&w=majority`;
 
 // const url = 'mongodb://127.0.0.1:27017'
 
-const port = 8000
+const port = 3000;
 
 // const dbName = 'LZW'
 // let db
 
-MongoClient
-    .connect(uri, {
-        useNewUrlParser: true,
-        maxPoolSize: 50,
-        wtimeoutMS: 2500
-    })
-    .catch(err => {
-        console.error(err.stack)
-        process.exit(1)
-    })
-    .then(async client => {
-        await compressDAO.injectDB(client)
-        app.listen(port, () => {
-            console.log(`Listening on port ${port}`)
-        })
-    })
+MongoClient.connect(uri, {
+  useNewUrlParser: true,
+  maxPoolSize: 50,
+  wtimeoutMS: 2500,
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await compressDAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  });
