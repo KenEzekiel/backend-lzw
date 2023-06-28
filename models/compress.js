c8795f3e52827df5c715c90baed0af71c255b6fd
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-06-28 22:14:43 +0800

    rm: unused

--- models/compress.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require("mongoose");
-
-// const compressSchema = mongoose.Schema({
-//     text: { type: String, required: true },
-//     compressed: { type: String, required: true }
-// });
-
-// module.exports = mongoose.model("Compress", compressSchema);
